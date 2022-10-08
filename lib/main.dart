4491c2ae49baa6e5cbaab993ba85441902b0ca1d
SebiC98 <sebastiancioata98@gmail.com>
2022-10-08 12:27:47 +0300

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -57,7 +58,7 @@ class MyHomePage extends StatelessWidget {
                         border: Border.all(color: Colors.purple, width: 2)),
                     padding: EdgeInsets.all(10),
                     child: Text(
-                      tx.amount.toString(),
+                      '\$${tx.amount}',
                       style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
@@ -73,7 +74,7 @@ class MyHomePage extends StatelessWidget {
                             fontSize: 16, fontWeight: FontWeight.bold),
                       ),
                       Text(
-                        tx.date.toString(),
+                        DateFormat.yMMMMd().format(tx.date),
                         style: TextStyle(color: Colors.grey),
                       )
                     ],
